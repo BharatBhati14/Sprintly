57eea4a5f5d42a576fee2723dcc5f328d08e5bfb
BharatBhati14 <bhatibharatkumar333@gmail.com>
2026-08-30 17:42:53 +0530

    Initialize Sprintly - a multi-tenant project-management system

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Srintly</h1>
+      <p>a multi-tenant project-management system</p>
+    </div>
+  );
+}
